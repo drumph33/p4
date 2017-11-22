ab236ad4f2b6b3242da7ba2c00eed95ebaf06518
Dylan Rumph <dprumph@gmail.com>
2017-11-22 12:54:25 -0600

    added seeders

---
+++ database/seeds/ScoresTableSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Score;
+
+class ScoresTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $scores = [
+            [99, 45],
+            [33, 13],
+            [4, 1],
+            [26, 22],
+            [8, 10],
+            [74, 29],
+            [42, 23],
+            [1, 0],
+            [11, 5],
+            [18, 9]
+        ];
+
+        $count = count($scores);
+
+        foreach ($scores as $key => $score) {
+            Score::insert([
+                'created_at' => Carbon\Carbon::now()->subDays($count)->toDateTimeString(),
+                'updated_at' => Carbon\Carbon::now()->subDays($count)->toDateTimeString(),
+                'date' => Carbon\Carbon::now()->subDays($count)->toDateTimeString(),
+                'anxiety' => $score[0],
+                'depression' => $score[1]
+            ]);
+            $count--;
+        }
+    }
+}
---
+++ resources/views/form.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.master')
+
+@section('title')
+    Anxiety and Depression Surveys
+@endsection
+
+@section('content')
+    <div class='container'>
+        <div class='panel-group'>
+            <div class='panel panel-info'>
+                <div class="panel-heading">
+                    <h2> Burns Anxiety Inventory
+                </div>
+                <div class="panel-body">
+                    <form method='GET' action='/directions'>
+
+            <div class='panel panel-info'>
+                <div class="panel-heading">
+                    <h2> Burns Depression Checklist
+                </div>
+                <div class="panel-body">
+                    <form method='GET' action='/directions'>
